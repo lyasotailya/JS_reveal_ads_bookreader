ebecdb86402f570a47c93004ef9013668cc79863
Irina <i.liasota@ya.ru>
2024-07-21 15:35:05 +0500

    Second commit

--- Bookreader_JS/task.js
+++ Bookreader_JS/task.js
@@ -2,24 +2,22 @@ let a = document.querySelectorAll('a.font-size')
 let book = document.querySelector('div.book')
 
 a.forEach((el) => {
-    el.onclick = () => {
+    el.onclick = (e) => {
+        e.preventDefault()
+
         let active = document.querySelector('a.font-size_active')
-        active.classList.toggle('font-size_active')
-
-        let small = document.querySelector('div.book_fs-small')
-        let big = document.querySelector('div.book_fs-big')
-        small.classList.remove('book_fs-small')
-        big.classList.remove('book_fs-big')
-
-        el.classList.remove('font-size_active')
+        active.classList.remove('font-size_active')
         
-        if (a.indexOf(el) == 0) {
-            book.classList.add('book_fs-small')
-        }
+        el.classList.add('font-size_active')
 
-        if (a.indexOf(el) == 2) {
-            book.classList.add('book_fs-big')
+        if (el.classList.contains('font-size_small')) {
+            book.className = 'book book_fs-small'
+        } else if (el.classList.contains('font-size_big')) {
+            book.className = 'book book_fs-big'
+        } else {
+            book.className = 'book'
+        }
         }
     }
-})
+)
 
